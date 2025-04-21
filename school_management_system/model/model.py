dae0296c4abfe791867d9d4af6529ca51dcf88e2
Mantu105 <127162724+Mantu105@users.noreply.github.com>
2025-04-21 18:49:10 +0530

    Update model.py
    for the comment delete

--- school_management_system/model/model.py
+++ school_management_system/model/model.py
@@ -2,12 +2,9 @@
 from odoo import models, fields
 
 class CommonDetails(models.AbstractModel):
-    """
-    Description : Creating the Abstract model which contain the common feilds for both the students and the teacher
-    
-    """
     _name = "common.details"
     _description = "Contain Common Informations"
+    
     name = fields.Char("Name")
     age = fields.Integer("Age")
     date_of_birth = fields.Date("Date of Birth")
