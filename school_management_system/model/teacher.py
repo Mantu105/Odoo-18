baf2fa51ad226f3f2adecd66800bcd9d07edaddf
Mantu105 <127162724+Mantu105@users.noreply.github.com>
2025-04-21 18:50:33 +0530

    Update teacher.py
    some change

--- school_management_system/model/teacher.py
+++ school_management_system/model/teacher.py
@@ -1,9 +1,6 @@
 from odoo import models ,fields
 
 class TeacherDetail(models.Model):
-    """
-    Description: Student Class which is used to store Teacher information
-    """
     _name = "teacher.information"
     _description = "This model is used to store the information of the teacher"
     _inherit = 'common.details'
